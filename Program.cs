4659e274771b68cdbfc5e5d9a53265e1e25561bf
shin0112 <wnslcosltimo12@gmail.com>
2025-09-30 13:28:10 +0900

    style: console writeline 함수 1줄에서 2줄로 변경, switch문 continue -> break로 변경

--- Program.cs
+++ Program.cs
@@ -24,7 +24,8 @@ namespace TEXT_RPG
             // STEP 1
             while (true)
             {
-                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.\n이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
+                Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+                Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
                 WriteSelection(SceneType.Start);
 
                 select = SelectAct(SceneType.Start);
@@ -40,7 +41,7 @@ namespace TEXT_RPG
                         break;
                     default:
                         WarnBadInput();
-                        continue;
+                        break;
                 }
             }
         }
