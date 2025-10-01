f3067ca10b6e97754efe21727f29f1f72b08da8a
shin0112 <wnslcosltimo12@gmail.com>
2025-10-01 12:09:10 +0900

    feat: 게임 메니저 생성

---
+++ GameManager.cs
@@ -0,0 +1,27 @@
+﻿using TEXT_RPG.Core;
+
+namespace TEXT_RPG
+{
+    internal class GameManager
+    {
+        public Player Player { get; }
+        public Shop Shop { get; }
+        public Dictionary<SceneType, string[]> Scenes { get; }
+
+        public GameManager()
+        {
+            Player = new("아무개", PlayerJob.마법사);
+            Shop = new();
+            Scenes = new Dictionary<SceneType, string[]>
+            {
+                { SceneType.Start, ["", "상태 보기", "인벤토리", "랜덤 모험", "마을 순찰하기", "훈련하기", "상점"] },
+                { SceneType.Status, ["나가기"] },
+                { SceneType.Inventory, ["나가기", "장착 관리", "아이템 정렬"] },
+                { SceneType.InventoryManagement, ["나가기"] },
+                { SceneType.InventorySort, ["나가기", "이름", "장착순", "공격력", "방어력"] },
+                { SceneType.Shop, ["나가기", "아이템 구매"] },
+                { SceneType.ShopPurchase, ["나가기"] }
+            };
+        }
+    }
+}
