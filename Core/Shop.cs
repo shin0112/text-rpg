8c0cb3f90a91967b5e84ad15d535d56db54434c6
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 20:44:29 +0900

    refactor: 접근 제어자 변경

--- Core/Shop.cs
+++ Core/Shop.cs
@@ -5,7 +5,7 @@ namespace TEXT_RPG.Core
 {
     internal class Shop
     {
-        public List<ShopEntry> ShopEntries { get; set; } = new();
+        public List<ShopEntry> ShopEntries { get; private set; } = new();
 
         public void InitializeDefaultItems()
         {
