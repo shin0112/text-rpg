5f3edf71668d71f1530ba419e2831ecf8de658f5
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 09:48:32 +0900

    feat: 아이템 판매 용 상점 ui 만들기

--- UI/ShopUI.cs
+++ UI/ShopUI.cs
@@ -4,19 +4,47 @@
     {
         public static void ShowShop(string title)
         {
+            GameManager manager = GameManager.Instance;
+
             UIHelper.WriteTitle(title);
             Console.WriteLine("필요한 아이템을 필요한 아이템을 얻을 수 있는 상점입니다.\n");
 
             Console.WriteLine("[보유 골드]");
-            Console.WriteLine($"{GameManager.Instance.Player.Gold} G\n");
+            Console.WriteLine($"{manager.Player.Gold} G\n");
 
             Console.WriteLine("[아이템 목록]");
-            for (int i = 0; i < GameManager.Instance.Shop.ShopEntries.Count; i++)
+            if (manager.InventoryNumbered)
             {
-                var shopEntry = GameManager.Instance.Shop.ShopEntries[i];
+                ShowSellShopEntry(manager);
+            }
+            else
+            {
+                ShowShopEntry(manager);
+            }
+        }
+
+        private static void ShowSellShopEntry(GameManager manager)
+        {
+            var items = manager.Player.Items;
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.Write($"- {i + 1} ");
+                ItemUI.ShowItemInfo(items[i]);
+                Console.Write($" | {(int)(items[i].Price * 0.85)} G");
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowShopEntry(GameManager manager)
+        {
+
+            for (int i = 0; i < manager.Shop.ShopEntries.Count; i++)
+            {
+                var shopEntry = manager.Shop.ShopEntries[i];
 
                 Console.Write("- ");
-                if (GameManager.Instance.ShopNumbered)
+                if (manager.ShopNumbered)
                 {
                     Console.Write($"{i + 1} ");
                 }
