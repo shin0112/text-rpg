21a6243b190f692821726a3adaa89b867a5bb7cf
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 11:11:54 +0900

    feat: 던전 클리어 데이터 임시 저장용 dto 만들기

---
+++ Core/DTO/DungeonResultDto.cs
@@ -0,0 +1,16 @@
+﻿namespace TEXT_RPG.Core.DTO
+{
+    internal class DungeonResultDto
+    {
+        public int HpLost { get; set; }
+        public int RewardGold { get; set; }
+        public int RewardExp { get; set; }
+
+        public DungeonResultDto(int hpLost, int rewardGold, int rewardExp)
+        {
+            HpLost = hpLost;
+            RewardGold = rewardGold;
+            RewardExp = rewardExp;
+        }
+    }
+}
