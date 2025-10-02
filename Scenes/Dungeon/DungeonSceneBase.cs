da80265f6b1a6461e81f0ef42c70604f9e84fd86
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 12:14:17 +0900

    feat: 던전용 UI 따로 제작

--- Scenes/Dungeon/DungeonSceneBase.cs
+++ Scenes/Dungeon/DungeonSceneBase.cs
@@ -8,7 +8,7 @@ namespace TEXT_RPG.Scenes.Dungeon
         {
             UIHelper.WriteTitle(Title);
             Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
-            UIHelper.WriteOptions();
+            DungeonUI.WriteOptions();
             int select = Manager.SelectAct();
             HandleInput(select);
         }
---
+++ UI/DungeonUI.cs
@@ -0,0 +1,20 @@
+﻿using TEXT_RPG.Repository;
+using TEXT_RPG.Scenes;
+
+namespace TEXT_RPG.UI
+{
+    internal class DungeonUI
+    {
+        public static void WriteOptions()
+        {
+            Scene scene = GameManager.Instance.CurrentScene;
+            for (int i = 1; i < scene.Options.Length; i++)
+            {
+                UIHelper.WriteOption($"{i}. {UIHelper.GetPaddedString(scene.Options[i], 15)} " +
+                    $"| 방어력 {DungeonRepository.Dungeons[(DungeonLevel)i].RequiredDef} 이상 권장");
+            }
+
+            UIHelper.WriteExitOption();
+        }
+    }
+}
