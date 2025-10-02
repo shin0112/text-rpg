a3891c12596d8e43af43d0837f22c643be7e9b20
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 12:14:38 +0900

    fix: 생성자 방어력, 공격력 위치 재설정

--- Core/DungeonInfo.cs
+++ Core/DungeonInfo.cs
@@ -2,15 +2,15 @@
 {
     internal class DungeonInfo
     {
-        public int RequiredAtk { get; set; }
         public int RequiredDef { get; set; }
+        public int RequiredAtk { get; set; }
         public int RewardGold { get; set; }
         public int RewardExp { get; set; }
 
-        public DungeonInfo(int requiredAtk, int requiredDef, int rewardGold, int rewardExp)
+        public DungeonInfo(int requiredDef, int requiredAtk, int rewardGold, int rewardExp)
         {
-            RequiredAtk = requiredAtk;
             RequiredDef = requiredDef;
+            RequiredAtk = requiredAtk;
             RewardGold = rewardGold;
             RewardExp = rewardExp;
         }
--- Repository/DungeonRepository.cs
+++ Repository/DungeonRepository.cs
@@ -6,6 +6,7 @@ namespace TEXT_RPG.Repository
     {
         public static readonly Dictionary<DungeonLevel, DungeonInfo> Dungeons = new()
         {
+            // 방어력, 공격력, 골드, 경험치
             { DungeonLevel.쉬운, new DungeonInfo(5, 0, 1000, 50) },
             { DungeonLevel.일반, new DungeonInfo(11, 0, 1700, 100) },
             { DungeonLevel.어려운, new DungeonInfo(17, 0, 2500, 200) }
