ec45ef5fea2b9e89d0fddd3a6ef50ce25d5f28f9
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 12:46:50 +0900

    style: 라인 변경

--- UI/RestUI.cs
+++ UI/RestUI.cs
@@ -5,7 +5,8 @@
         public static void ShowRest(string title)
         {
             UIHelper.WriteTitle(title);
-            Console.WriteLine($"500 G를 내면 체력을 회복할 수 있습니다. (보유 골드 : {GameManager.Instance.Player.Gold} G)");
+            Console.WriteLine("500 G를 내면 체력을 회복할 수 있습니다. " +
+                $"(보유 골드 : {GameManager.Instance.Player.Gold} G)\n");
             UIHelper.WriteOptions();
         }
     }
