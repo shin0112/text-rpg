211ae1ff4d72e9a10f3db62de9062c81e0ddc33a
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 11:09:23 +0900

    feat: 던전 화면 베이스 클래스 만들기

---
+++ Scenes/Dungeon/DungeonSceneBase.cs
@@ -0,0 +1,16 @@
+﻿using TEXT_RPG.UI;
+
+namespace TEXT_RPG.Scenes.Dungeon
+{
+    internal abstract class DungeonSceneBase : Scene
+    {
+        public override void Show()
+        {
+            UIHelper.WriteTitle(Title);
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            UIHelper.WriteOptions();
+            int select = Manager.SelectAct();
+            HandleInput(select);
+        }
+    }
+}
