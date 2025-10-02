4c1e77010a6f0ccb42da8e4d80c04673a52bb38e
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 12:02:36 +0900

    feat: 던전 클리어 화면 만들기

--- Scenes/Dungeon/DungeonClearScene.cs
+++ Scenes/Dungeon/DungeonClearScene.cs
@@ -1,4 +1,7 @@
-﻿namespace TEXT_RPG.Scenes.Dungeon
+﻿using TEXT_RPG.Core.DTO;
+using TEXT_RPG.UI;
+
+namespace TEXT_RPG.Scenes.Dungeon
 {
     internal class DungeonClearScene : DungeonSceneBase
     {
@@ -11,6 +14,23 @@
             {
                 Manager.ChangeScene(SceneType.Start);
             }
+        }
+
+        public override void Show()
+        {
+            DungeonResultDto dto = Manager.LastDungeonResult!;
+
+            UIHelper.WriteTitle(Title);
+            Console.WriteLine("축하합니다!!");
+            Console.WriteLine($"{dto.Level} 던전을 클리어 하였습니다.");
+
+            Console.WriteLine("[탐험 결과]");
+            Console.WriteLine($"체력 {dto.BeforeHp} -> {dto.AfterHp}");
+            Console.WriteLine($"Gold {dto.BeforeGold} G -> {dto.AfterGold} G");
+
+            UIHelper.WriteOptions();
+            int select = Manager.SelectAct();
+            HandleInput(select);
         }
     }
 }
