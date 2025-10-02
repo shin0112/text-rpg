cf3a0ff9bfce3f6312066f395f80045134b888ce
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 14:04:59 +0900

    feat: 레벨 당 요구 경험치 저장용 repo 만들기

---
+++ Data/LevelRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace TEXT_RPG.Data
+{
+    internal class LevelRepository
+    {
+        public static readonly Dictionary<int, int> RequiredExp = new()
+        {
+            { 1, 50 },
+            { 2, 80 },
+            { 3, 150 },
+            { 4, 500 },
+        };
+    }
+}
