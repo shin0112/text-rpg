d18285e350ff48196f9f6451fd7e71dc1b551da4
shin0112 <wnslcosltimo12@gmail.com>
2025-10-02 11:11:23 +0900

    feat: 고정된 던전 정보 저장용 클래스 및 repo 만들기

---
+++ Core/DungeonInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace TEXT_RPG.Core
+{
+    internal class DungeonInfo
+    {
+        public int RequiredAtk { get; set; }
+        public int RequiredDef { get; set; }
+        public int RewardGold { get; set; }
+        public int RewardExp { get; set; }
+
+        public DungeonInfo(int requiredAtk, int requiredDef, int rewardGold, int rewardExp)
+        {
+            RequiredAtk = requiredAtk;
+            RequiredDef = requiredDef;
+            RewardGold = rewardGold;
+            RewardExp = rewardExp;
+        }
+    }
+}
---
+++ Repository/DungeonRepository.cs
@@ -0,0 +1,14 @@
+﻿using TEXT_RPG.Core;
+
+namespace TEXT_RPG.Repository
+{
+    internal class DungeonRepository
+    {
+        public static readonly Dictionary<DungeonLevel, DungeonInfo> Dungeons = new()
+        {
+            { DungeonLevel.Easy, new DungeonInfo(5, 0, 1000, 50) },
+            { DungeonLevel.Normal, new DungeonInfo(11, 0, 1700, 100) },
+            { DungeonLevel.Hard, new DungeonInfo(17, 0, 2500, 200) }
+        };
+    }
+}
