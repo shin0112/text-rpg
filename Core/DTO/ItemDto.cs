da9c29626fc6b6530bdc7b127bc5d84f4a26c5ad
shin0112 <wnslcosltimo12@gmail.com>
2025-10-06 09:57:58 +0900

    feat: player, item dto 작성

---
+++ Core/DTO/ItemDto.cs
@@ -0,0 +1,14 @@
+﻿using TEXT_RPG.Data;
+
+namespace TEXT_RPG.Core.DTO
+{
+    internal record class ItemDto
+    {
+        public string Name { get; init; } = string.Empty;
+        public ItemType Type { get; init; }
+        public int Value { get; init; }
+        public string Description { get; init; } = string.Empty;
+        public bool IsEquipped { get; init; }
+        public int Price { get; init; }
+    }
+}
---
+++ Core/DTO/PlayerDto.cs
@@ -0,0 +1,19 @@
+﻿using TEXT_RPG.Data;
+
+namespace TEXT_RPG.Core.DTO
+{
+    internal record class PlayerDto
+    {
+        public int Level { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public PlayerJob Job { get; init; }
+        public float AttackPower { get; init; }
+        public float DefensePower { get; init; }
+        public int Hp { get; init; }
+        public int Stamina { get; init; }
+        public int Exp { get; init; }
+        public int Gold { get; init; }
+        public List<ItemDto> Items { get; init; } = new();
+        public Dictionary<ItemType, ItemDto?> Equipped { get; init; } = new();
+    }
+}
